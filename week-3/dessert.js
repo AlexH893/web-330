a2c226512e991074647d9031190eaf24da601ef8
Alex <31377763+AlexH893@users.noreply.github.com>
2021-04-04 20:41:18 -0500

    dessert.js fixes

--- week-3/dessert.js
+++ week-3/dessert.js
@@ -2,7 +2,7 @@
         //Import statement for the product object.
         import { Product } from "./product.js";
                         //dessert?
-        export class MainCourse extends Product {
+        export class Dessert extends Product {
 
             constructor(name, price) {
 
