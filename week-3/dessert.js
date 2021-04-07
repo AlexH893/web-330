74523685b3ed10c24784e2f228da46719256e338
Alex <31377763+AlexH893@users.noreply.github.com>
2021-04-06 20:22:56 -0500

    css import change

--- week-3/dessert.js
+++ week-3/dessert.js
@@ -6,7 +6,7 @@
 */
         //Import statement for the product object.
         import { Product } from "./product.js";
-                        //dessert?
+                      
         export class Dessert extends Product {
 
             constructor(name, price) {
