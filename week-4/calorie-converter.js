17436a93ca95b23948494b98ba703bb3b9ca6cde
Alex <31377763+AlexH893@users.noreply.github.com>
2021-04-07 20:33:19 -0500

    added calorie-converter.js

---
+++ week-4/calorie-converter.js
@@ -0,0 +1,36 @@
+/*
+https://www.w3schools.com/jsref/jsref_includes.asp
+*/
+    import { FoodModel } from "./food-model.js";
+
+    export class CalorieConverter {
+
+        //Populating static variable.
+        static data = [
+
+        Egg = {ID: 1007, Name: ID, Calories: 78},
+        Apple = {ID: 1008, Name: Apple, Calories: 95},
+        Hamburger = {ID: 1009, Name: Hamburger, Calories: 354},
+        Fries = {ID: 1010, Name: Fries, Calories: 400},
+        Banana = {ID: 1011, Name: Banana, Calories: 105},
+        Soda = {ID: 1012, Name: Soda, Calories: 150}
+
+        ];
+
+
+        static find(string) {
+
+            return newArray = this.data.filter(FoodModel.name == string);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
