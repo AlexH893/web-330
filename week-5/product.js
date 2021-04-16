0bbae0abfa3a55e56389f8ff2b56d2e83966b16e
Alex <31377763+AlexH893@users.noreply.github.com>
2021-04-16 13:56:50 -0500

    product.js update
    added exported product class w/constructor and params.

---
+++ week-5/product.js
@@ -0,0 +1,22 @@
+/** 
+   Title: product.js
+   Author: Alex Haefner
+   Date: 4-16-2021
+   Description: Class 
+*/
+
+export class Product {
+
+    constructor(name, price){
+
+        this.name = name;
+
+        this.price = price;
+
+        //Add a new property for id
+
+        this.id = Math.random().toString(16).slice(2);
+
+    }
+
+}
