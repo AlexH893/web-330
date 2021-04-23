606f57b36cf2d1fccb10cfe8990540d1ec34cd30
Alex <31377763+AlexH893@users.noreply.github.com>
2021-04-23 11:54:21 -0500

    Update validator.js

--- week-6/validator.js
+++ week-6/validator.js
@@ -3,4 +3,22 @@ import { FloatField } from "./float-field.js";
 import { FloatMinField } from "./float-min-field.js";
 import { FloatMaxField } from "./float-max-field.js";
 
+class validator {   
+    validators = [];
+    messages = [];
 
+    constructor(name, field) {
+        
+        this.name = name;
+
+        this.field = field;
+
+}
+
+    addRequiredField() {
+
+        
+
+    }
+
+}
