b1fa2e8e709661cb17b0550c0013d24a30ab0937
Alex <31377763+AlexH893@users.noreply.github.com>
2021-04-23 11:54:11 -0500

    add validator.js

---
+++ week-6/validator.js
@@ -0,0 +1,6 @@
+import { RequiredField } from "./required-field.js";
+import { FloatField } from "./float-field.js";
+import { FloatMinField } from "./float-min-field.js";
+import { FloatMaxField } from "./float-max-field.js";
+
+
