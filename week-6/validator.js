8e1faae56aaafa389451aa4f0295b96e595cfa88
Alex <31377763+AlexH893@users.noreply.github.com>
2021-04-25 20:08:23 -0500

    exporting class

--- week-6/validator.js
+++ week-6/validator.js
@@ -10,7 +10,7 @@ import { FloatField } from "./float-field.js";
 import { FloatMinField } from "./float-min-field.js";
 import { FloatMaxField } from "./float-max-field.js";
 
-class validator {   
+export class validator {   
     validators = [];
     messages = [];
 
