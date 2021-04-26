b8446b934253ded5b587081db9af48a8d74d7fa3
Alex <31377763+AlexH893@users.noreply.github.com>
2021-04-25 19:59:28 -0500

    import fix

--- week-6/validator.js
+++ week-6/validator.js
@@ -5,7 +5,7 @@
    Description: Validator class with functions
 
 */
-import { RequiredField, RequiredField } from "./required-field.js";
+import { RequiredField} from "./required-field.js";
 import { FloatField } from "./float-field.js";
 import { FloatMinField } from "./float-min-field.js";
 import { FloatMaxField } from "./float-max-field.js";
