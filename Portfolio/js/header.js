1cb7afaadfc52717dbe8509835f4846ff248ab9c
Alex <31377763+AlexH893@users.noreply.github.com>
2021-05-04 16:58:35 -0500

    navbar fixes
    uniform sizing & positioning

--- Portfolio/js/header.js
+++ Portfolio/js/header.js
@@ -7,17 +7,21 @@ class Header extends HTMLElement {
     connectedCallback() {
 
         this.innerHTML = `
-            <html>
-            <head>
-            <meta charset="UTF-8">
-            <title>title</title>
-            <link rel="stylesheet" href="css/styles.css">
-            <script src="js/footer.js"></script>
-            <script src="https://use.fontawesome.com/d4276bd064.js"></script>
-            <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-            <script src="js/FadeTransition.js"></script>
-            </head>`;
+        <div class="col w-25">
+        <div class="navbar navbar-inverse navbar-fixed-left mt-4">
+           <a href="index.html">
+           <img class="navImg" src="img/signature.png"></img>
+           </a>
+           <ul class="nav navbar-nav">
+               <li><a href="about.html">About</a></li>
+               <li><a href="resume.html">Resume</a></li>
+               <li><a href="projects.html">Projects</a></li>
+               <li><a href="databasediagram.html">Database Diagrams</a></li>
+               <li><a href="apiunittests.html">API Unit Tests</a></li>
+               <li><a href="#">Bucket List</a></li>
+           </ul>
+        </div>
+     </div>`;
 
     }
 }
