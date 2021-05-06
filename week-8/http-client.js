d24f8d3b0b53b9834cd742da43d85901c494a0ff
Alex <31377763+AlexH893@users.noreply.github.com>
2021-05-05 22:46:08 -0500

    initial commit

---
+++ week-8/http-client.js
@@ -0,0 +1,33 @@
+export class HttpClient {
+
+    async get(url, params) {
+
+        params = "";
+
+        //Instantiate new URL object, supply it the url parameter, assign the results to the url parameter
+        url = new URL(url);
+
+        url.search = new URLSearchParams(params);
+
+        const res = await fetch(url.toString(), {
+
+            method: "GET"
+
+            .then(response => response.json())
+
+            //needs finishing
+
+        })
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
