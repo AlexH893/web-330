4df1454e4112a12e51c5034c87500e5b8d89aa3b
Alex <31377763+AlexH893@users.noreply.github.com>
2021-05-05 19:39:17 -0500

    inline styling links

--- Portfolio/js/header.js
+++ Portfolio/js/header.js
@@ -13,12 +13,12 @@ class Header extends HTMLElement {
            <img class="navImg" src="img/signature.png"></img>
            </a>
            <ul class="nav navbar-nav">
-               <li><a href="about.html">About</a></li>
-               <li><a href="resume.html">Resume</a></li>
-               <li><a href="projects.html">Projects</a></li>
-               <li><a href="databasediagram.html">Database Diagrams</a></li>
-               <li><a href="apiunittests.html">API Unit Tests</a></li>
-               <li><a href="#">Bucket List</a></li>
+               <li><a href="about.html" style="color: #634240">About</a></li>
+               <li><a href="resume.html" style="color: #634240">Resume</a></li>
+               <li><a href="projects.html" style="color: #634240">Projects</a></li>
+               <li><a href="databasediagram.html" style="color: #634240">Database Diagrams</a></li>
+               <li><a href="apiunittests.html" style="color: #634240">API Unit Tests</a></li>
+               <li><a href="bucketlist.html#" style="color: #634240">Bucket List</a></li>
            </ul>
         </div>
      </div>`;
