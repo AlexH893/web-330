877e5c06747fbfbf975c2ccfb7d6e61ae0ac6610
Alex <31377763+AlexH893@users.noreply.github.com>
2021-05-10 22:33:28 -0500

    added email link in footer

--- Portfolio/js/footer.js
+++ Portfolio/js/footer.js
@@ -7,7 +7,7 @@ class Footer extends HTMLElement
     connectedCallback() {
         this.innerHTML = `
         <div class="footer">
-        <i class="fa fa-envelope" aria-hidden="true"></i>
+        <a href="mailto:alexhaefner@live.com"><i class="fa fa-envelope" aria-hidden="true"></i></a>
         <a href="https://www.linkedin.com/in/alexhaefner/" target='blank'><img src="img/linkedin.svg" width="16px"></img></a>
      </div>
         `;
