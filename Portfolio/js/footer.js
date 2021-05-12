61706b3230a9ab621330daf681c83e3b906815cd
Alex <31377763+AlexH893@users.noreply.github.com>
2021-05-11 23:44:28 -0500

    updates

--- Portfolio/js/footer.js
+++ Portfolio/js/footer.js
@@ -7,8 +7,14 @@ class Footer extends HTMLElement
     connectedCallback() {
         this.innerHTML = `
         <div class="footer">
-        <a href="mailto:alexhaefner@live.com"><i class="fa fa-envelope" aria-hidden="true"></i></a>
-        <a href="https://www.linkedin.com/in/alexhaefner/" target='blank'><img src="img/linkedin.svg" width="16px"></img></a>
+        <a href="mailto:alexhaefner@live.com"><img src="img/email.svg" title="Email me" width="16px"></img></i></a> /
+        <a href="https://www.linkedin.com/in/alexhaefner/" title="My Linkedin" target='blank'><img src="img/linkedin.svg" width="16px"></img></a> /
+        <a href="https://www.youtube.com/user/bellevueuniversity" title="Bellevue University's Youtube"  target='blank'><img src="img/youtube.svg" width="16px"></img></a> 
+        <br>
+        <a href="https://github.com/AlexH893" title="My GitHub"  target='blank'><img src="img/github.svg" width="16px"></img></a> /
+        <a href="https://github.com/buwebdev" title="Courses Repository"  target='blank'><img src="img/github.svg" width="16px"></img></a>
+
+
      </div>
         `;
     }
--- Portfolio/js/header.js
+++ Portfolio/js/header.js
@@ -8,17 +8,17 @@ class Header extends HTMLElement {
 
         this.innerHTML = `
         <div class="col w-25">
-        <div class="navbar navbar-inverse navbar-fixed-left mt-4">
+        <div class="navbar navbar-fixed-left mt-4">
            <a href="index.html">
            <img class="navImg" src="img/sig.png"></img>
            </a>
-           <ul class="nav navbar-nav">
-               <li><a href="about.html" style="color: #634240">About</a></li>
-               <li><a href="resume.html" style="color: #634240">Resume</a></li>
-               <li><a href="projects.html" style="color: #634240">Projects</a></li>
-               <li><a href="databasediagram.html" style="color: #634240">Database Diagrams</a></li>
-               <li><a href="apiunittests.html" style="color: #634240">API Unit Tests</a></li>
-               <li><a href="bucketlist.html#" style="color: #634240">Bucket List</a></li>
+           <ul class="navList">
+               <li><a class="link link-ltr" href="about.html" >About</a></li>
+               <li><a class="link link-ltr" href="resume.html">Resume</a></li>
+               <li><a class="link link-ltr" href="projects.html">Projects</a></li>
+               <li><a class="link link-ltr" href="databasediagram.html">Database Diagrams</a></li>
+               <li><a class="link link-ltr" href="apiunittests.html">API Unit Tests</a></li>
+               <li><a class="link link-ltr" href="bucketlist.html#">Bucket List</a></li>
            </ul>
         </div>
      </div>`;
